661321765dd26984386d5c12602fe2d894825ceb
ghalandary <ab.ghalandary@outlook.com>
2025-11-14 22:02:38 +0330

    implement Q1

--- src/two_wheel_robot/launch/display.launch.py
+++ src/two_wheel_robot/launch/display.launch.py
@@ -1,34 +1,25 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import Command, PathJoinSubstitution
+from launch.substitutions import Command
+import os
 
 def generate_launch_description():
-    xacro_file = PathJoinSubstitution([
-        FindPackageShare('two_wheel_robot'),
-        'urdf',
-        'two_wheel_robot.xacro'
-    ])
 
-    robot_description_content = Command(['xacro ', xacro_file])
-
-    rviz_config_file = PathJoinSubstitution([
-        FindPackageShare('two_wheel_robot'),
-        'rviz',
-        'display.rviz'
-    ])
+    pkg_share = FindPackageShare('two_wheel_robot').find('two_wheel_robot')
+    xacro_file = os.path.join(pkg_share, 'urdf', 'two_wheel_robot.xacro')
+    rviz_config_file = os.path.join(pkg_share, 'rviz', 'display.rviz')
 
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': robot_description_content}],
+        parameters=[{'robot_description': Command(['xacro ', xacro_file])}],
         output='screen'
     )
 
     node_joint_state_publisher_gui = Node(
         package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui'
+        executable='joint_state_publisher_gui'
     )
 
     node_rviz = Node(
